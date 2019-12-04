f3f2d0d1c43b8250e44e15030d1432b27c4b7c6c
Eason Lin <uu9924079@gmail.com>
2019-12-04 14:47:50 +0800

    feat: require bodyParser and methodOverride

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.listen(port, () => {
   console.log(`app is running at ${port}`)
