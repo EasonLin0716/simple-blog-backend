56adc84d82dfb9f6f5c19e28cb94215d655fd143
Eason Lin <uu9924079@gmail.com>
2019-12-04 14:00:11 +0800

    feat: add static file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`app is running at ${port}`)
