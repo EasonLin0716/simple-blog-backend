eb000afb0a6934e5e25492a6acc572a4a3d951ac
Eason Lin <uu9924079@gmail.com>
2019-12-16 16:50:34 +0800

    chore: install jsonwebtaoken, passport-jwt and POST /api/signin

--- routes/apis.js
+++ routes/apis.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const postController = require('../controllers/apis/postController')
 const replyController = require('../controllers/apis/replyController')
+const userController = require('../controllers/apis/userController')
 
 router.get('/', (req, res) => res.redirect('/apis/posts'))
+router.post('/signin', userController.signIn)
 router.get('/posts', postController.getPosts)
 router.get('/posts/:id', postController.getPost)
 router.get('/posts/:id/replies', replyController.getReplies)
