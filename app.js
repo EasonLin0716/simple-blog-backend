96463ca67de67c18441a843c63ef1440d367c0b8
Eason Lin <uu9924079@gmail.com>
2020-01-06 17:24:22 +0800

    fix: port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const upload = multer()
 const flash = require('connect-flash')
 const session = require('express-session')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 
 if (process.env.NODE_ENV !== 'production') {
